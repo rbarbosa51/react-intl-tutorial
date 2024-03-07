4b247e0d41734b9fb3bc08575c16eebea1683e1c
Rafael Barbosa <rafael.barbosa51@gmail.com>
2024-03-06 19:01:20 -0600

    Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,19 @@
 import React from "react";
-import { IntlProvider, FormattedMessage } from "react-intl";
-import { useState } from "react";
-import English from "./languages/en";
-import Spanish from "./languages/es";
 
 function App() {
-  const [msgs, setMsgs] = useState(English);
-
-  const changeLanguage = () => {
-    if (msgs === English) {
-      setMsgs(Spanish);
-    } else {
-      setMsgs(English);
-    }
-  };
   return (
-    <IntlProvider locale="en" messages={msgs}>
-      <div className="h-screen bg-gradient-to-b from-orange-50 to-rose-300 text-rose-800/60 my-4">
-        <h1 className="text-center font-inter text-5xl font-extrabold text-rose-300">
-          <FormattedMessage id="appTitle" defaultMessage={"Hello World"} />
-        </h1>
-        <div className="mt-4 flex flex-col items-center gap-8">
-          <p className="text-center">
-            <FormattedMessage id="appText" defaultMessage={English.appText} />
-          </p>
-          <p className="text-center w-1/2">
-            <FormattedMessage id="appQuote" defaultMessage={English.appQuote} />
-          </p>
-          <button
-            className="w-64 rounded-md border border-solid border-rose-500 p-4"
-            onClick={changeLanguage}
-          >
-            <FormattedMessage
-              id="appButton"
-              defaultMessage={English.appButton}
-            />
-          </button>
-        </div>
+    <div className="my-4 h-screen bg-gradient-to-b from-orange-50 to-rose-300 text-rose-800/60">
+      <h1 className="text-center font-inter text-5xl font-extrabold text-rose-300">
+        Sample Title
+      </h1>
+      <div className="mt-4 flex flex-col items-center gap-8">
+        <p className="text-center">Sample paragraph</p>
+        <p className="w-1/2 text-center">Sample Quote</p>
+        <button className="w-64 rounded-md border border-solid border-rose-500 p-4">
+          Sample Button Name
+        </button>
       </div>
-    </IntlProvider>
+    </div>
   );
 }
 
